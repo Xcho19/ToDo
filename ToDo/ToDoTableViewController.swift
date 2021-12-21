477143f310a9e14b8714edc0e7caacb2b2618c0b
Xcho <xach55@gmail.com>
2021-12-21 12:42:50 +0400

    Add part 6

--- ToDo/ToDoTableViewController.swift
+++ ToDo/ToDoTableViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 class ToDoTableViewController: UITableViewController {
     
     var toDos = [ToDo]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         toDos = ToDo.loadToDos() ?? ToDo.loadToDosSamples()
         navigationItem.leftBarButtonItem = editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
-
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDos.count
     }
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            return true
+        return true
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             toDos.remove(at: indexPath.row)
@@ -41,11 +41,19 @@ class ToDoTableViewController: UITableViewController {
         
         let toDo = toDos[indexPath.row]
         cell.textLabel?.text = toDo.title
-
+        
         return cell
     }
     
     @IBAction func unwindToToDoList(segue: UIStoryboardSegue){
+        guard segue.identifier == "saveUnwind" else { return }
+        let sourceViewController = segue.source as! ToDoDetailTableViewController
         
+        if let todo = sourceViewController.todo {
+            let newIndexPath = IndexPath(row: toDos.count, section: 0)
+            
+            toDos.append(todo)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
     }
 }
